de6d752d2a9a90b4790d266893cb5f5e8338ca61
brachsmithi <bsmithi.impaired@gmail.com>
2021-01-09 10:52:58 -0500

    extract expanding results to new separate components

--- src/results/SearchResult.js
+++ src/results/SearchResult.js
@@ -2,47 +2,65 @@ import React, { useState } from 'react';
 
 export default class SearchResult extends React.Component {
 
-  state = { showAlternateTitles: false, showDirectorAliases: false };
-
   render() {
-    const onAlternateTitleClick = () => this.setState(prev => ({ showAlternateTitles: !prev.showAlternateTitles}))
-    const onDirectorAliasClick = () => this.setState(prev => ({ showDirectorAliases: !prev.showDirectorAliases}))
     return (
       <>
-        <div className="result-name">
-          {this.props.title.name}
-          {this.props.title.alternateTitles &&
-            <div className="alternate-title-toggle" onClick={onAlternateTitleClick}>
-              {this.state.showAlternateTitles ? "less" : "more"}
-            </div>
-          }
-          {this.state.showAlternateTitles &&
-              <div className="alternate-titles">
-                {this.props.title.alternateTitles.map((title, i) => [
-                  i > 0 && ", ",
-                  title 
-                ])}
-              </div>
-          }
-        </div>
+        <TitleResult {...this.props.title} />
         <div className="result-year">({this.props.year})</div>
-        <div className="result-director">
-          {this.props.director.name}
-          {this.props.director.aliases &&
-            <div className="director-alias-toggle" onClick={onDirectorAliasClick}>
-              {this.state.showDirectorAliases ? "less" : "more"}
-            </div>
-          }
-          {this.state.showDirectorAliases &&
-              <div className="director-aliases">
-                {this.props.director.aliases.map((alias, i) => [
-                  i > 0 && ", ",
-                  alias 
-                ])}
-              </div>
-          }
-        </div>
+        <DirectorResult {...this.props.director} />
       </>
     );
+  }
+}
+
+export class TitleResult extends React.Component {
+  state = { show: false };
+
+  render() {
+    const onClick = () => this.setState(prev => ({ show: !prev.show}))
+    return (
+      <div className="result-name">
+        {this.props.name}
+        {this.props.alternateTitles &&
+          <div className="alternate-title-toggle" onClick={onClick}>
+            {this.state.show ? "less" : "more"}
+          </div>
+        }
+        {this.state.show &&
+            <div className="alternate-titles">
+              {this.props.alternateTitles.map((title, i) => [
+                i > 0 && ", ",
+                title 
+              ])}
+            </div>
+        }
+      </div>
+    )
+  }
+}
+
+export class DirectorResult extends React.Component {
+  state = { show: false };
+  
+  render() {
+    const onClick = () => this.setState(prev => ({ show: !prev.show}))
+    return (
+      <div className="result-director">
+        {this.props.name}
+        {this.props.aliases &&
+          <div className="director-alias-toggle" onClick={onClick}>
+            {this.state.show ? "less" : "more"}
+          </div>
+        }
+        {this.state.show &&
+            <div className="director-aliases">
+              {this.props.aliases.map((alias, i) => [
+                i > 0 && ", ",
+                alias 
+              ])}
+            </div>
+        }
+      </div>
+    )
   }
 }
