45e2340fac94559e77cb46952dd5c5b6a5fa117b
brachsmithi <bsmithi.impaired@gmail.com>
2021-03-05 19:10:14 -0500

    add stub submission handler to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import SearchButton from './form/SearchButton';
 import SearchResultSection from './results/SearchResultSection';
 
 function App() {
+  const searchFormHandler = () => {alert('button clicked')}
   return (
     <div className="App">
       <h1>Program Search Portal</h1>
-      <SearchForm>
+      <SearchForm onSubmit={searchFormHandler}>
         <SearchField />
         <SearchButton />
       </SearchForm>
